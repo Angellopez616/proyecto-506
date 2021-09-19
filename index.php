4b1a82805a83be87943f88492c63c3b7ad17547f
Angellopez616 <91029687+Angellopez616@users.noreply.github.com>
2021-09-19 16:15:58 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>inicio de Sistema bootstrap5</title>
+    <link rel=" stylesheet"type="text/css" href="css\bootstrap.min.css">
+  </head>
+  <body>
+    <h1>hola</h1>
+  </body>
+</html>
